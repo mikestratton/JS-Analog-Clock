3032c8ec0df466068c93a1f2c82dfa8504aaaf35
mikestratton <mike@mikestratton.net>
2022-08-17 19:15:12 -0400

    analog clock in js

---
+++ assets/app.js
@@ -0,0 +1,23 @@
+const handSeconds = document.getElementById('hand-seconds');
+const handMinutes = document.getElementById('hand-minutes');
+const handHours = document.getElementById('hand-hours');
+
+function getTime(){
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    const timeInterval = 6;
+
+    handSeconds.style.transform = 'rotate(' + (seconds * timeInterval) + 'deg)';
+    handMinutes.style.transform = 'rotate(' + (minutes * timeInterval) + 'deg)';
+    handHours.style.transform = 'rotate(' + (hours * 30 + minutes / 2) + 'deg)';
+
+    console.log(now);
+    console.log(hours);
+
+}
+
+// setInterval(getTime, 100)
+getTime();
